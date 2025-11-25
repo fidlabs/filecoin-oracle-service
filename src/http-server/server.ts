124c4ecc607f916e798e3d75a0b00a03b02cc09e
lukas <lukasz.wal@neti-soft.com>
2025-11-25 12:40:19 +0100

    Adjust transaction for a new SLIs from CDP enpoint

--- src/http-server/server.ts
+++ src/http-server/server.ts
@@ -45,7 +45,11 @@ app.post(
     httpLogger.info("Manual trigger received via /trigger-now");
 
     try {
+      // await getSPEmptyAttestations();
+
       await setSliOracleJob();
+
+      // await getSPFillAttestations();
       res.json({ status: "ok", message: "Job triggered successfully" });
     } catch (err) {
       const message = err instanceof Error ? err.message : String(err);
